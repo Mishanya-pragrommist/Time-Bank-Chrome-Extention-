e99157f3be3b32c0495b8b9051487cde6d944082
Mishanya <chuvachock1521@gmail.com>
2026-02-16 20:38:26 +0200

    Created basic version of method add() of class Time

--- extention/js/classes.js
+++ extention/js/classes.js
@@ -6,12 +6,21 @@ class Time {
     }
     
     add(time) {
-        if (time.hours > 0 && time.minutes > 0 && time.seconds > 0) {
-            this.seconds += time.seconds;
-            this.minutes += this.seconds / 60; 
-            
-            this.hours = time.hours;
+        this.seconds += time.seconds;
+        if (this.seconds >= 60) {
+            const minutes = Math.floor(this.seconds / 60);
+            this.seconds -= 60 * minutes;
+            this.minutes += minutes;
         }
+        
+        this.minutes += time.minutes;
+        if (this.minutes >= 60) {
+            const hours =  Math.floor(this.minutes / 60);
+            this.minutes -= 60 * hours;
+            this.hours += hours;
+        }
+        
+        this.hours += time.hours;
     }
     
     substract(time) {
@@ -19,14 +28,18 @@ class Time {
     }
     
     round() {
-        
+        this.seconds = 0;
     }
     
     toString() {
-        // Вспомогательная функция для добавления нуля (pad)
-        // Если число 5 -> "05", если 12 -> "12"
+        // For adding 0 (pad)
+        // Eg,  5 -> "05", 12 -> "12"
         const pad = (num) => String(num).padStart(2, '0');
 
         return `${pad(this.hours)}:${pad(this.minutes)}:${pad(this.seconds)}`;
     }
 }
+
+const test = new Time(1, 20, 20);
+
+console.log(time);
