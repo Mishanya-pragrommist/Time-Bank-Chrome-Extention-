f361b6a25538392e2aec00d63d08c32dfe75280a
Mishanya <chuvachock1521@gmail.com>
2026-02-17 16:48:22 +0200

    Written method add() for class Time

--- extention/js/classes.js
+++ extention/js/classes.js
@@ -1,45 +1,67 @@
 class Time {
-    constructor(hours, minutes, seconds) {
-        this.hours = hours;
-        this.minutes = minutes;
-        this.seconds = seconds;
+    constructor(hours, minutes, seconds, time) {
+        if (time !== undefined) {
+            this.seconds = time.seconds;
+            this.minutes = time.minutes;
+            this.hours = time.hours;
+        }
+        else {
+            this.hours = hours;
+            this.minutes = minutes;
+            this.seconds = seconds;
+        }
     }
     
-    add(time) {
-        this.seconds += time.seconds;
+    add(seconds, minutes, hours) {
+        this.seconds += seconds;
         if (this.seconds >= 60) {
-            const minutes = Math.floor(this.seconds / 60);
-            this.seconds -= 60 * minutes;
-            this.minutes += minutes;
+            const extraMinutes = Math.floor(this.seconds / 60);
+            this.seconds -= 60 * extraMinutes;
+            this.minutes += extraMinutes;
         }
         
-        this.minutes += time.minutes;
-        if (this.minutes >= 60) {
-            const hours =  Math.floor(this.minutes / 60);
-            this.minutes -= 60 * hours;
+        if (minutes !== undefined) {
+            this.minutes += minutes;
+            if (this.minutes >= 60) {
+                const extraHours =  Math.floor(this.minutes / 60);
+                this.minutes -= 60 * extraHours;
+                this.hours += extraHours;
+            }
+        }
+        
+        if (hours !== undefined) {
             this.hours += hours;
         }
-        
-        this.hours += time.hours;
     }
     
-    substract(time) {
-        this.seconds -= time.seconds;
+    substract(seconds, minutes, hours) {
+        if (hours > this.hours) throw new Exception("Not enough hours");
+        this.hours -= hours;
+        this.minutes -= minutes;
+        this.seconds -= seconds;
     }
     
-    round() {
+    floor() {
         this.seconds = 0;
+    }
+    
+    getTime() {
+        return new Time(this);
     }
     
     toString() {
         // For adding 0 (pad)
         // Eg,  5 -> "05", 12 -> "12"
         const pad = (num) => String(num).padStart(2, '0');
-
         return `${pad(this.hours)}:${pad(this.minutes)}:${pad(this.seconds)}`;
     }
 }
 
 const test = new Time(1, 20, 20);
 
-console.log(time);
+console.log(test.toString());
+
+test.add(250, 120);
+console.log("1:20:20 + 240s 120m = ", test.toString());
+
+test.substract();
