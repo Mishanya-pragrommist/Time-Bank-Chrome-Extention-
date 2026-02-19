a69f464a9f8af19b7b399f09e9e1e2ccbbf3f823
Mishanya <chuvachock1521@gmail.com>
2026-02-19 09:54:12 +0200

    Created basic version of adding bonus time block

--- extention/js/main.js
+++ extention/js/main.js
@@ -1,4 +1,18 @@
 import { Time } from "./classes.js";
 
 const time = new Time(1, 20, 20);
+console.log(time.toString());
+const account = document.querySelector("[main-account]");
+const bonus = document.querySelector("[bonus-account]");
+const addButton = document.querySelector("[add-button]");
+const cancelButton = document.querySelector("[cancel-button]");
+const bonusDescription = document.querySelector("[bonus-description]");
 
+account.textContent = time.toString();
+bonus.textContent = time.toString();
+
+addButton.addEventListener("click", () => {
+    console.log("blyat");
+    cancelButton.style.display = "block";
+    bonusDescription.style.display = "block";
+});
