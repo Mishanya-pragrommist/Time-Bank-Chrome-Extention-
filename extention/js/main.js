abe716f18f837942bed8d9f3ed46a2a1e44a941f
Mishanya <chuvachock1521@gmail.com>
2026-02-19 16:54:32 +0200

    Started creating scrypt for adding bonus time through modal window

--- extention/js/main.js
+++ extention/js/main.js
@@ -1,18 +1,19 @@
 import { Time } from "./classes.js";
 
 const time = new Time(1, 20, 20);
-console.log(time.toString());
+
 const account = document.querySelector("[main-account]");
 const bonus = document.querySelector("[bonus-account]");
 const addButton = document.querySelector("[add-button]");
-const cancelButton = document.querySelector("[cancel-button]");
-const bonusDescription = document.querySelector("[bonus-description]");
+const modalWindow = document.querySelector("[modal-window]");
 
 account.textContent = time.toString();
 bonus.textContent = time.toString();
 
+console.log(time.toString());
+
 addButton.addEventListener("click", () => {
-    console.log("blyat");
-    cancelButton.style.display = "block";
-    bonusDescription.style.display = "block";
+    console.log("add button works");
+    const response = fetch("../html/popups/add bonus.html");
+    
 });
