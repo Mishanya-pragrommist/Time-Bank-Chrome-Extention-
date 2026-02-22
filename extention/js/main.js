5ae4e24b4852bdb016f5e54074b7422ba81f23fc
Mishanya <chuvachock1521@gmail.com>
2026-02-22 10:29:35 +0200

    Now time is gotten from single input. Also a bug appeared: if at least one of the numbers of timeInputField is null, then other field is ignored and time is not added

--- extention/js/main.js
+++ extention/js/main.js
@@ -13,7 +13,7 @@ const modalWindow = document.querySelector("[modal-window]");
 account.textContent = time.toString();
 bonusAccount.textContent = bonus.toString();
 
-//Shows modal window
+//Opens modal window
 addButton.addEventListener("click", () => {
     noteText.style.display = "none";
     modalWindow.style.display = "flex";
@@ -25,42 +25,62 @@ const cancelButtonWindow = modalWindow.querySelector("[cancel-btn]");
 
 // Fields in modal window to fill
 const bonusDescField = modalWindow.querySelector("[bonus-desc]");
-const minutesField = modalWindow.querySelector("[minutes-js]");
-const secondsField = modalWindow.querySelector("[seconds-js]");
+const timeInputField = modalWindow.querySelector("[time-input]");
 
+// Text that shows up if:
+//      not enough time on bonus account;
+//      user entered more than 20 minutes
 const noteText = modalWindow.querySelector("[note-js]");
-// For closing window
+
+// === For closing window ===
 
 // Removes text in inputs and makes window invisible
 function closeModal() {
     modalWindow.style.display = "none";
     bonusDescField.value = '';
-    minutesField.value = 0;
-    secondsField.value = 0;
+    timeInputField.value="00:00";
 }
 
+// To submit adding time
 submitButtonWindow.addEventListener("click", () => {
     // Getting data from fields
-    const minutes = Number.parseInt(minutesField.value);
-    const seconds = Number.parseInt(secondsField.value);
+    const timeInput = timeInputField.value.split(":");
+    let minutes = Number.parseInt(timeInput[0]);
+    let seconds = Number.parseInt(timeInput[1]);
     const bonusDesc = bonusDescField.value;
     
+    // Check if numbers are not numbers
+    if (Number.isNaN(minutes)) minutes = 0;
+    if (Number.isNaN(seconds)) seconds = 0;
+    
+    // If user entered no time, just close window
+    if (minutes === 0 && seconds === 0) {
+        closeModal();
+        return;
+    } 
+    
+    // Check if user entered more than 20 minutes 
+    // (20mins * 60s = 1200s).
+    // If yes, show note about it
     if (minutes * 60 + seconds > 1200) {
+        noteText.textContent = "Можно добавить не более 20 минут за раз";
         noteText.style.display = "block";
         return;
     }
     
     // Check if we have enough time on bonus account
     if (bonus.toSeconds() < seconds + minutes * 60) {
+        noteText.textContent = "Бонусов недостаточно";
         noteText.style.display = "block";
         console.log("Not enough time on bonus account");
         return;
     }
     
-    // Add time to account and substract it from bonusAccount 
+    // Add time to main account 
     time.add(seconds, minutes); // No need for adding hours
     account.textContent = time.toString();
     
+    // Substract time from bonus account
     bonus.substract(seconds, minutes);
     bonusAccount.textContent = bonus.toString();
     
